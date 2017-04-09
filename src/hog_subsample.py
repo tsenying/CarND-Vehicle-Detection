2a7762d88669d1fb3a7f91706e76c250202da03f
Ying Hong <ying_hong@trimble.com>
2017-04-08 21:07:54 -0600

    fix indentation

--- src/hog_subsample.py
+++ src/hog_subsample.py
@@ -25,68 +25,71 @@ img = mpimg.imread('test_images/test4.jpg')
 
 # Define a single function that can extract features using hog sub-sampling and make predictions
 def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
+    
+    draw_img = np.copy(img)
+    # Uncomment the following line if you extracted training
+    # data from .png images (scaled 0 to 1 by mpimg) and the
+    # image you are searching is a .jpg (scaled 0 to 255)
+    #img = img.astype(np.float32)/255
+    
+    img_tosearch = img[ystart:ystop,:,:]
+    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1]/scale), np.int(imshape[0]/scale)))
 
- draw_img = np.copy(img)
- #img = img.astype(np.float32)/255
+    ch1 = ctrans_tosearch[:,:,0]
+    ch2 = ctrans_tosearch[:,:,1]
+    ch3 = ctrans_tosearch[:,:,2]
 
- img_tosearch = img[ystart:ystop,:,:]
- ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
- if scale != 1:
-     imshape = ctrans_tosearch.shape
-     ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1]/scale), np.int(imshape[0]/scale)))
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell)-1
+    nyblocks = (ch1.shape[0] // pix_per_cell)-1 
+    nfeat_per_block = orient*cell_per_block**2
+    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell)-1 
+    cells_per_step = 2  # Instead of overlap, define how many cells to step
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
 
- ch1 = ctrans_tosearch[:,:,0]
- ch2 = ctrans_tosearch[:,:,1]
- ch3 = ctrans_tosearch[:,:,2]
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
 
- # Define blocks and steps as above
- nxblocks = (ch1.shape[1] // pix_per_cell)-1
- nyblocks = (ch1.shape[0] // pix_per_cell)-1 
- nfeat_per_block = orient*cell_per_block**2
- # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
- window = 64
- nblocks_per_window = (window // pix_per_cell)-1 
- cells_per_step = 2  # Instead of overlap, define how many cells to step
- nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
- nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+             ypos = yb*cells_per_step
+             xpos = xb*cells_per_step
+             # Extract HOG for this patch
+             hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+             hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+             hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+             hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
 
- # Compute individual channel HOG features for the entire image
- hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
- hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
- hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+             xleft = xpos*pix_per_cell
+             ytop = ypos*pix_per_cell
 
- for xb in range(nxsteps):
-     for yb in range(nysteps):
-         ypos = yb*cells_per_step
-         xpos = xb*cells_per_step
-         # Extract HOG for this patch
-         hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
-         hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
-         hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
-         hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+             # Extract the image patch
+             subimg = cv2.resize(ctrans_tosearch[ytop:ytop+window, xleft:xleft+window], (64,64))
 
-         xleft = xpos*pix_per_cell
-         ytop = ypos*pix_per_cell
+             # Get color features
+             spatial_features = bin_spatial(subimg, size=spatial_size)
+             hist_features = color_hist(subimg, nbins=hist_bins)
 
-         # Extract the image patch
-         subimg = cv2.resize(ctrans_tosearch[ytop:ytop+window, xleft:xleft+window], (64,64))
+             # Scale features and make a prediction
+             test_features = X_scaler.transform(np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))    
+             #test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))    
+             test_prediction = svc.predict(test_features)
 
-         # Get color features
-         spatial_features = bin_spatial(subimg, size=spatial_size)
-         hist_features = color_hist(subimg, nbins=hist_bins)
+             if test_prediction == 1:
+                 xbox_left = np.int(xleft*scale)
+                 ytop_draw = np.int(ytop*scale)
+                 win_draw = np.int(window*scale)
+                 cv2.rectangle(draw_img,(xbox_left, ytop_draw+ystart),(xbox_left+win_draw,ytop_draw+win_draw+ystart),(0,0,255),6) 
 
-         # Scale features and make a prediction
-         test_features = X_scaler.transform(np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))    
-         #test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))    
-         test_prediction = svc.predict(test_features)
-
-         if test_prediction == 1:
-             xbox_left = np.int(xleft*scale)
-             ytop_draw = np.int(ytop*scale)
-             win_draw = np.int(window*scale)
-             cv2.rectangle(draw_img,(xbox_left, ytop_draw+ystart),(xbox_left+win_draw,ytop_draw+win_draw+ystart),(0,0,255),6) 
-
- return draw_img
+    return draw_img
 
 ystart = 400
 ystop = 656
